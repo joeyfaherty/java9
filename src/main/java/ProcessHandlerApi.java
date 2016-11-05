1af57028e3f570bcb2125987289e284dff328a8c
joeyfaherty <joeyfaherty@live.ie>
2016-11-05 12:42:10 +0100

    Add comments to ProcessHandlerApi class

--- src/main/java/ProcessHandlerApi.java
+++ src/main/java/ProcessHandlerApi.java
@@ -1,6 +1,12 @@
 import java.io.IOException;
 import java.util.stream.Stream;
 
+/**
+ * ProcessHandle identifies and provides control of native processes. Each individual process
+ * can be monitored for liveness, list its children, get information about the process or destroy it.
+ *
+ * http://download.java.net/java/jdk9/docs/api/java/lang/ProcessHandle.html
+ */
 public class ProcessHandlerApi {
 
     public static void main(String[] args) throws IOException {
@@ -18,16 +24,17 @@ public class ProcessHandlerApi {
 
     private static void getPidOfCurrentProcess() throws IOException {
         ProcessHandle current = ProcessHandle.current();
+        // Returns the native process ID of the process
         long pid = current.getPid();
         System.out.println("Current pid is " + pid);
     }
 
     private static void getProcessInformation() {
         ProcessHandle currentProcess = ProcessHandle.current();
-
-        // get info from current process
+        // Information snapshot about the process.
+        // The attributes of a process vary by operating system and are not available in all implementations
+        // Return types are all Optional<T>
         ProcessHandle.Info processInfo = currentProcess.info();
-
         System.out.println(String.format("Process [%s] started at [%s] by user [%s] " +
                         "from path [%s] took [%s] milliseconds to execute",
                 currentProcess.getPid(),
@@ -44,12 +51,14 @@ public class ProcessHandlerApi {
     }
 
     private static void getAllProcesses() {
+        // Returns a snapshot of all processes visible to the current process.
         Stream<ProcessHandle> processHandleStream = ProcessHandle.allProcesses();
         System.out.println("List of all current processes are : ");
         processHandleStream.forEach(System.out::println);
     }
 
     private static void printProcessesThatHaveChildren() {
+        // e.children() returns a snapshot of the current direct children of the process.
         ProcessHandle.allProcesses()
                 // keep only elements that have at least one child
                 .filter(e -> e.children().count() >= 1)
